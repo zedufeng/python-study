07aed072f08cc1abb3b35679f98e9eb43e412667
zedufeng <2420315561@qq.com>
2026-03-21 00:53:47 +0800

    提交说明

---
+++ day2.py
@@ -0,0 +1,10 @@
+a=int(input('first number'))
+b=int(input('second number'))
+c=int(input('third number'))
+if(a>b)and(a>c):
+    answer=a
+elif(b>a)and(b>c):
+    answer=b
+else:
+    answer=c
+print(answer)
--- print.py
+++ print.py
@@ -1,1 +1,5 @@
-print(2026)
+n=5
+while n>0:
+    print(n)
+    n=n-1
+print('Blastoff')
